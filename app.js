b400b8780de4e7ad0e75a6b7f0a3fc2e9c2f712a
Blaze Smith <blazesmith@Blazes-MBP.attlocal.net>
2022-04-03 11:47:28 -0500

    Angular folder deleted, dist files in correct folders

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const cors = require("cors");
 const bodyParser = require("body-parser");
 
 const app = express();
